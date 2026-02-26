2dcb38d2b5533257f5d5c94dd79ee3ce97897a20
Matias Pallero <matuutepallero@gmail.com>
2026-02-26 20:58:24 -0300

    otros de app,js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://instalar.netlify.app/',
   credentials: true
 }));
 app.use(express.json());
